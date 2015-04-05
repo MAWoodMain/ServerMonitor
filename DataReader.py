409b4329c34a1b0eb7d18740c3fc7232c3297dc1
MAWood <mattcow12@googlemail.com>
2015-04-06 00:53:09 +0100

    Class structure cleaned - OVERHAULED

--- DataReader.py
+++ DataReader.py
@@ -5,42 +5,44 @@ import time
 import Globals
 
 
-class DataReader():
-    def __init__(self, scene):
-        self.scene = scene
-        self.terminated = False
+def __init__(self, scene):
+    self.scene = scene
+    self.terminated = False
 
-        self.on = False
+    self.on = False
+    self.cpu_load = 0.0
+    self.mem_total = 0
+    self.mem_free = 0
+
+
+def terminate(self):
+    self.terminated = True
+
+
+def __call__(self):
+    while not (self.terminated or Globals.TERMINATING):
+        # Read channel 0 in single-ended mode using the settings above
+        self.update_info()
+        self.scene.update_cpu(self.cpu_load)
+        self.scene.update_mem(self.mem_free, self.mem_total)
+        time.sleep(Globals.UPDATE_DELAY)
+
+
+def update_info(self):
+    self.on = bool(os.popen(Globals.ON_COMMAND).read())
+    if self.on:
+        self.cpu_load = float(os.popen(
+            "ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.CPU_LOAD_COMMAND).read()) * Globals.CPU_LOAD_COEFFICIENT
+        self.mem_total = int(os.popen(
+            "ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_TOTAL_COMMAND).read()) * Globals.MEM_TOTAL_COEFFICIENT
+        self.mem_free = int(
+            os.popen("ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_FREE_COMMAND).read())
+        self.mem_free += int(
+            os.popen("ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_BUFFERS_COMMAND).read())
+        self.mem_free += int(
+            os.popen("ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_CACHED_COMMAND).read())
+        self.mem_free *= Globals.MEM_FREE_COEFFICIENT
+    else:
         self.cpu_load = 0.0
         self.mem_total = 0
         self.mem_free = 0
-
-    def terminate(self):
-        self.terminated = True
-
-    def __call__(self):
-        while not (self.terminated or Globals.TERMINATING):
-            # Read channel 0 in single-ended mode using the settings above
-            self.update_info()
-            self.scene.update_cpu(self.cpu_load)
-            self.scene.update_mem(self.mem_free, self.mem_total)
-            time.sleep(Globals.UPDATE_DELAY)
-
-    def update_info(self):
-        self.on = bool(os.popen(Globals.ON_COMMAND).read())
-        if self.on:
-            self.cpu_load = float(os.popen(
-                "ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.CPU_LOAD_COMMAND).read()) * Globals.CPU_LOAD_COEFFICIENT
-            self.mem_total = int(os.popen(
-                "ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_TOTAL_COMMAND).read()) * Globals.MEM_TOTAL_COEFFICIENT
-            self.mem_free = int(
-                os.popen("ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_FREE_COMMAND).read())
-            self.mem_free += int(
-                os.popen("ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_BUFFERS_COMMAND).read())
-            self.mem_free += int(
-                os.popen("ssh " + Globals.USERNAME + "@" + Globals.HOST + Globals.MEM_CACHED_COMMAND).read())
-            self.mem_free *= Globals.MEM_FREE_COEFFICIENT
-        else:
-            self.cpu_load = 0.0
-            self.mem_total = 0
-            self.mem_free = 0
