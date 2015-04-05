257f3237afcd876f8079d803e50c6b78930c182a
MAWood <mattcow12@googlemail.com>
2015-04-05 23:13:54 +0100

    Scene pop test

--- ServerMonitor.py
+++ ServerMonitor.py
@@ -7,13 +7,13 @@ UPDATE_DELAY = 1
 
 ON_COMMAND = "ping -c 1 192.168.1.176 | grep 'packets transmitted' |  cut -c 24"
 
-CPU_LOAD_COMMAND = "\"cat /proc/loadavg\" | cut -c 1-4"
+CPU_LOAD_COMMAND = " \"cat /proc/loadavg\" | cut -c 1-4"
 CPU_LOAD_COEFFICIENT = 0.125
 
-MEM_TOTAL_COMMAND = "\"grep MemTotal /proc/meminfo\" | awk '{print $2}'"
+MEM_TOTAL_COMMAND = " \"grep MemTotal /proc/meminfo\" | awk '{print $2}'"
 MEM_TOTAL_COEFFICIENT = 1.0
 
-MEM_FREE_COMMAND = "\"grep MemFree /proc/meminfo\" | awk '{print $2}'"
+MEM_FREE_COMMAND = " \"grep MemFree /proc/meminfo\" | awk '{print $2}'"
 MEM_FREE_COEFFICIENT = 1.0
 
 import pygame
